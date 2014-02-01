f18b2049053cfb6b34a1410d940cff8885231e7a
previ <roberto.previtera@gmail.com>
2014-02-01 21:24:40 +0100

    first commit

---
+++ js/coderbot.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$('#b_forward').on("click", function (){
+		alert("Forward");
+	});
+	$('#b_left').on("click", function (){
+		alert("Left");
+	});
+	$('#b_right').on("click", function (){
+		alert("Right");
+	});
+	$('#b_backward').on("click", function (){
+		alert("Backward");
+	});
+});
