10cb230f40de3dc6a0971f05cdf6dd610ed16678
previ <roberto.previtera@gmail.com>
2014-02-01 23:13:55 +0000

    py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 import coderbot
 app = Flask(__name__,static_url_path="")
@@ -9,18 +9,18 @@ def home():
     return render_template('control.html')
 
 @app.route("/bot")
-def bot():
-    cmd = self.request.get('cmd')
-    param = self.request.get('param')
+def handle_bot():
+    cmd = request.args.get('cmd')
+    param = request.args.get('param')
     if cmd == "forward":
         bot.forward(float(param))
     elif cmd == "left":
         bot.left(float(param))
     elif cmd == "right":
         bot.right(float(param))
-    elif cmd == "left":
+    elif cmd == "backward":
         bot.backward(float(param))
-    return
+    return "ok"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
