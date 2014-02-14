2e985a7a8da26a4e985ce2c2f1e0b6f7d4da3627
previ <roberto.previtera@gmail.com>
2014-02-14 22:44:21 +0100

    -

--- init.py
+++ init.py
@@ -1,10 +1,13 @@
-import signal_reader
+import camera_handler
 import main
 
-reader = signal_reader.SignalReader.get_instance()
+camera_handler = camera_handler.CameraReader.get_instance()
+camera_handler.add_handler(camera_handler.SignalReader())
+camera_handler.add_handler(camera_handler.LogoReader())
+camera_handler.set_active_handler(None)
 
 if __name__=="__main__":
-  reader.start()
+  camera_handler.start()
   main.run_server()
   reader.join()
 
