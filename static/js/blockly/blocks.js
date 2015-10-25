1e684cf63e049af2e5315d70fe845b3dbc8f3a65
previ <roberto.previtera@gmail.com>
2015-10-25 09:05:38 +0000

    -

--- static/js/blockly/blocks.js
+++ static/js/blockly/blocks.js
@@ -1,5 +1,6 @@
 /**
-  * Copyright (C) 2014 Roberto Previtera
+  * CoderBot, a didactical, programmable robot
+  * Copyright (C) 2014,2015  Roberto Previtera <info@coderbot.org>
   *
   * This program is free software: you can redistribute it and/or modify
   * it under the terms of the GNU General Public License as published by
