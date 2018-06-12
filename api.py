b052584a97e4c2f75a52a4d895c2d4f7cf687d18
Antonio Vivace <avivace4@gmail.com>
2018-06-12 17:36:59 +0200

    /move method, specification and example values. Stub /status

--- api.py
+++ api.py
@@ -5,6 +5,7 @@ from flask import jsonify
 import json
 from coderbot import CoderBot
 from config import Config
+import connexion
 bot_config = Config.get()
 bot = CoderBot.get_instance(servo=(bot_config.get("move_motor_mode") == "servo"),
 									 motor_trim_factor=float(bot_config.get('move_motor_trim', 1.0)))
@@ -13,6 +14,10 @@ def stop():
 	bot.stop()
 	return "ok"
 
-def test():
-	print(json.dumps(request.args))
+def move(data):
+	print(data)
+	bot.move(speed=data['param1'], elapse=data['param2'])
+	return "ok"
+
+def status():
 	return "ok"
