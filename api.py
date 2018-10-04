a32793cc61d1585e21b5b2a1d409a25394ee5c52
Antonio Vivace <avivace4@gmail.com>
2018-10-04 17:06:43 +0200

    Add new exec endpoint, accepting JSON payloads instead of the legacy urlencoded payload

--- api.py
+++ api.py
@@ -4,11 +4,15 @@
 from flask import jsonify
 import json
 from coderbot import CoderBot
+from program import ProgramEngine, Program
 from config import Config
 import connexion
 bot_config = Config.get()
 bot = CoderBot.get_instance(servo=(bot_config.get("move_motor_mode") == "servo"),
 									 motor_trim_factor=float(bot_config.get('move_motor_trim', 1.0)))
+
+prog = None
+prog_engine = ProgramEngine.get_instance()
 
 def stop():
 	bot.stop()
@@ -26,3 +30,7 @@ def turn(data):
 
 def status():
 	return "ok"
+
+def exec(data):
+	prog = prog_engine.create(data["name"], data["code"])
+	return json.dumps(prog.execute())
