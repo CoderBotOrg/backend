ee7f78e67a43982cf77d58814fb2b05d4d10f969
Antonio Vivace <avivace4@gmail.com>
2018-11-22 17:24:27 +0100

    Improvements, comments, names, settings and persistent things in data/

--- api.py
+++ api.py
@@ -22,10 +22,13 @@ bot = CoderBot.get_instance(
 prog = None
 prog_engine = ProgramEngine.get_instance()
 
+programs = TinyDB("data/programs.json")
+activities = TinyDB("data/activities.json")
+
 
 def stop():
     bot.stop()
-    return "ok"
+    return 200
 
 
 def move(data):
@@ -40,12 +43,21 @@ def turn(data):
     return 200
 
 
+# Bot status (STUB)
 def status():
     return {
-        "status": "ok", "internetConnectivity": True, "temp": "40", "uptime": "5h", "status": "ok", "internetConnectivity": True, "temp": "40", "uptime": "5h"}
+        "status": "ok",
+        "internetConnectivity": True,
+        "temp": "40",
+        "uptime": "5h",
+        "status": "ok",
+        "internetConnectivity": True,
+        "temp": "40",
+        "uptime": "5h",
+    }
 
 
-# Hardware and software information
+# Hardware and software information (STUB)
 def info():
     return {
         "model": 1,
@@ -56,41 +68,47 @@ def info():
         "kernel": 6,
     }
 
+
 def exec(data):
     prog = prog_engine.create(data["name"], data["code"])
     return json.dumps(prog.execute())
 
-def editSettings(data):
-    return "ok"
 
 def restoreSettings():
-    with open('defaultConfig.json') as f:
+    with open("defaultConfig.json") as f:
         Config.write(json.loads(f.read()))
     bot_config = Config.get()
     return "ok"
 
 
-programs = TinyDB("programs.json")
-query = Query()
+## Programs
 
-def save(data):
-    if(programs.search(query.name == data["name"]) == []):
+
+def saveProgram(data):
+    if programs.search(query.name == data["name"]) == []:
         programs.insert(data)
+        return 200
     else:
-        if(programs.search((query.name == data["name"]) & (query.default == "True"))):
-            return "defaultOverwrite"
+        if programs.search((query.name == data["name"]) & (query.default == "True")):
+            return "defaultOverwrite", 400
         else:
             programs.update(data, query.name == data["name"])
+            return 200
 
-def load(name):
-    return programs.search(query.name == name)[0]
 
-def delete(data):
-    programs.remove(query.name == data["name"])
+def loadProgram(name):
+    return programs.search(query.name == name)[0], 200
 
-def list():
+
+def deleteProgram(data):
+    programs.remove(query.name == data["name"]), 200
+
+
+def listPrograms():
     return programs.all()
 
+
+# Delete everything but the defaults programs
 def resetDefaultPrograms():
     programs.purge()
     for filename in os.listdir("data"):
@@ -98,9 +116,3 @@ def resetDefaultPrograms():
             with open("data/" + filename) as p:
                 q = p.read()
                 programs.insert(json.loads(q))
-resetDefaultPrograms()
-#return programs.search(query.default == "True")
-
-
-settings = TinyDB("settings.json")
-settings.insert({"yessa": "okiz"})
