1a7f6b1c37e69575f692ae53f0c58692cbc09de9
Previtera Roberto <roberto.previtera@gmail.com>
2019-01-21 18:52:06 +0100

    added stub for testing #84

---
+++ test/stub/pigpio.py
@@ -0,0 +1,12 @@
+INPUT=0
+OUTPUT=1
+
+EITHER_EDGE = 2
+FALLING_EDGE = 1
+RISING_EDGE = 0
+
+class pi(object):
+    def __init__(self, host="localhost", port=8888):
+        pass
+
+
---
+++ test/stub/smbus2.py
@@ -0,0 +1,6 @@
+class SMBus(object):
+    def __init__(self, channel):
+        pass
+
+    def write_byte_data(self, a, b, c):
+        pass
