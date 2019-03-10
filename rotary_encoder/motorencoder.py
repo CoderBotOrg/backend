242ccd44eaec6582e6b7e71ef15db7f4261beb0d
sorre97 <sorre97@hotmail.it>
2019-03-10 19:47:05 +0100

    Single rotary motor class control finshed

--- rotary_encoder/motorencoder.py
+++ rotary_encoder/motorencoder.py
@@ -1,5 +1,6 @@
 import pigpio
 import threading
+from time import sleep
 
 from rotarydecoder import RotaryDecoder
 
@@ -14,9 +15,9 @@ class MotorEncoder:
         concurrency problems on GPIO READ/WRITE """
 
     # default constructor
-    def __init__(self, pi, enable_pin, forward_pin, backward_pin, encoder_feedback_pin):
+    def __init__(self, pigpio, enable_pin, forward_pin, backward_pin, encoder_feedback_pin):
         # setting pin variables
-        self._pi = pi
+        self._pigpio = pigpio
         self._enable_pin = enable_pin
         self._forward_pin = forward_pin
         self._backward_pin = backward_pin
@@ -26,14 +27,13 @@ class MotorEncoder:
         self._direction = 0
         self._distance = 0
         self._ticks = 0
-        self._PWM_value = 0
+        self._power = 0
         self._encoder_speed = 0
         self._is_moving = False
 
         # other
-        self._callback = self._pi.callback()
         self._motor_lock = threading.RLock()
-        self._rotary_decoder = RotaryDecoder(pi, encoder_feedback_pin, callback)
+        self._rotary_decoder = RotaryDecoder(pigpio, encoder_feedback_pin, self.rotary_callback)
 
     # GETTERS
     # ticks
@@ -59,32 +59,75 @@ class MotorEncoder:
     # MOVEMENT
     """ The stop function acquires the lock to operate on motor
         then writes a 0 on movement pins to stop the motor
-        and releases the lock afterwards """
-    def control(self, speed = 100.0):
+        and releases the lock afterwards 
+        Motor speed on range 0 - 100 already set on PWM_set_range(100)
+        if a time_elapse parameter value is provided, motion is locked
+        for a certain amount of time """
+
+    def control(self, power=100.0, time_elapse=0):
         self._motor_lock.acquire()  # acquiring lock
-        self._distance = 0  #resetting distance everytime motor is used
-        self._direction = 1 if speed > 0 else -1    # setting direction according to speed
+        self._direction = 1 if power > 0 else -1  # setting direction according to speed
+        self._power = power
+        self.stop()  # stopping motor to initialize new movement
 
-        if(self._direction):
+        # going forward
+        if (self._direction):
+            self._pigpio.set_PWM_dutycycle(self._forward_pin, abs(power))
+        # going bacakward
+        else:
+            self._pigpio.set_PWM_dutycycle(self._backward_pin, abs(power))
+
+        self._is_moving = True
+
+        # releasing lock on motor
+        self._motor_lock.release()
+
+        # movement time elapse
+        if (time_elapse > 0):
+            sleep(time_elapse)
             self.stop()
-            self._pigpio.set_PWM_dutycycle()
-        else
-            self.stop()
-            self._pigpio.set_PWM_dutycycle()
-
-
 
     """ The stop function acquires the lock to operate on motor
         then writes a 0 on movement pins to stop the motor
         and releases the lock afterwards """
+
     def stop(self):
         self._motor_lock.acquire()
-        self._pi.write(self._backward_pin, 0)
-        self._pi.write(self._forward_pin, 0)
-        self._is_moving = False
+
+        # stopping motor
+        self._pigpio.write(self._backward_pin, 0)
+        self._pigpio.write(self._forward_pin, 0)
+
+        # returning state variables to consistent state
+        self._distance = 0       # resetting distance travelled
+        self._ticks = 0          # resetting ticks
+        self._power = 0          # resetting PWM power
+        self._encoder_speed = 0  # resetting encoder speed
+        self._direction = 0      # resetting direction
+        self._is_moving = False  # resetting moving flag
+
+        # releasing lock
         self._motor_lock.release()
 
-    # OTHER
+    # CALLBACKS
+    """ The callback function rotary_callback is called on FALLING_EDGE by the
+        rotary_decoder with a parameter value of 1 (1 new tick)
+        - Gearbox ratio: 120:1 (1 wheel revolution = 120 motor revolution)
+        - Encoder ratio: 8:1 encoder ticks for 1 motor revolution
+        - 1 wheel revolution = 128 * 8 = 960 ticks
+        - R = 30mm
+        - 1 wheel revolution = 2πR = 2 * π * 30mm = 188.5mm
+        - 960 ticks = 188.5mm
+        - 1 tick = 0.196mm
+        - 1 tick : 0.196mm = x(ticks) : y(mm) """
+
+    # callback function
+    def rotary_callback(self, tick):
+        self._motor_lock.acquire()
+        self._ticks += tick  # updating ticks
+        # self._encoder_speed = ?    # encoder speed (mm/s)
+        self._distance = self._ticks * 0.196  # (mm) travelled
+
     # callback cancelling
     def cancel_callback(self):
         self._rotary_decoder.cancel()
--- rotary_encoder/rotarydecoder.py
+++ rotary_encoder/rotarydecoder.py
@@ -6,25 +6,28 @@ class RotaryDecoder:
 
    """ Class to decode mechanical rotary encoder pulses """
 
-   def __init__(self, pi, encoder_feedback_pin, callback):
+   def __init__(self, pigpio, encoder_feedback_pin, callback):
 
-      self.pi = pi
-      self.encoder_feedback_pin = encoder_feedback_pin   # encoder feedback pin
-      self.callback = callback   # callback function on event
-      self.value = 0             # value of encoder feedback
+      self._pigpio = pigpio
+      self._encoder_feedback_pin = encoder_feedback_pin   # encoder feedback pin
+      self._callback = callback   # callback function on event
+      self._value = 0             # value of encoder feedback
 
       # setting up GPIO
-      self.pi.set_mode(encoder_feedback_pin, pigpio.INPUT)
-      self.pi.set_pull_up_down(encoder_feedback_pin, pigpio.PUD_UP)
+      self._pigpio.set_mode(encoder_feedback_pin, pigpio.INPUT)
+      self._pigpio.set_pull_up_down(encoder_feedback_pin, pigpio.PUD_UP)
 
       # callback function on EITHER_EDGE
-      self.callback_trigger = self.pi.callback(encoder_feedback_pin, pigpio.EITHER_EDGE, self._pulse)
+      self._callback_trigger = self._pigpio.callback(encoder_feedback_pin, pigpio.EITHER_EDGE, self._pulse)
 
-   # pulse is the callback function on EITHER_EDGE
-   # it returns a 1 if the feedback square wave is on a falling edge
+   """ pulse is the callback function on EITHER_EDGE
+       it returns a 1 if the feedback square wave is on a falling edge 
+       It simply returns a 1 on FALLING EDGE, i've detached this class in order
+       to make it easier in case of extension with PIN A attached too """
    def _pulse(self, level):
       self.levB = level;
 
+      # RAISING EDGE
       if self.levB == 1:
           self.callback(1)
 
