9ea27eea45d7c931ba271459419992444686a3cf
previ <roberto.previtera@gmail.com>
2022-10-16 19:35:40 +0200

    wip #164

--- stub/alsaaudio/__init__.py
+++ stub/alsaaudio/__init__.py
@@ -1,4 +1,10 @@
 class Mixer():
+
+    PCM_FORMAT_U8 = 1
+    PCM_FORMAT_S16_LE = 2
+    PCM_FORMAT_S24_3LE = 3
+    PCM_FORMAT_S32_LE = 4
+
     def __init__(self, device, cardindex):
         pass
     
@@ -6,4 +12,12 @@ class Mixer():
         return 0.0
 
     def setvolume(self, volume):
+        pass
+
+
+class PCM():
+    def __init__(self, channels, rate, format, periodsize):
+        pass
+
+    def write(self, data):
         pass
