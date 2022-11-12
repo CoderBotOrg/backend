4f3009e58c4eb761db04f1d72eedaa508f49b5bb
Roberto <roberto.previtera@gmail.com>
2022-11-12 08:48:49 +0100

    add lock on TinyDB (#170)

--- coderbot/activity.py
+++ coderbot/activity.py
@@ -1,5 +1,5 @@
 from tinydb import TinyDB, Query
-
+from threading import Lock
 # Programs and Activities databases
 class Activities():
     _instance = None
@@ -13,34 +13,39 @@ class Activities():
     def __init__(self):
         self.activities = TinyDB("data/activities.json")
         self.query = Query()
+        self.lock = Lock()
 
     def load(self, name, default):
-        if name and default is None:
-            activities = self.activities.search(self.query.name == name)
-            if len(activities) > 0:
-                return activities[0]
-        elif default is not None:
-            if len(self.activities.search(self.query.default == True)) > 0:
-                return self.activities.search(self.query.default == True)[0]
+        with self.lock: 
+            if name and default is None:
+                activities = self.activities.search(self.query.name == name)
+                if len(activities) > 0:
+                    return activities[0]
+            elif default is not None:
+                if len(self.activities.search(self.query.default == True)) > 0:
+                    return self.activities.search(self.query.default == True)[0]
+                return None
             return None
-        return None
 
     def save(self, name, activity):
-        # if saved activity is "default", reset existing default activity to "non-default"
-        if activity.get("default", False) is True:
-            self.activities.update({'default': False})
-        if self.activities.search(self.query.name == name) == []:
-            self.activities.insert(activity)
-        else:
-            self.activities.update(activity, self.query.name == activity["name"])
+        with self.lock: 
+            # if saved activity is "default", reset existing default activity to "non-default"
+            if activity.get("default", False) is True:
+                self.activities.update({'default': False})
+            if self.activities.search(self.query.name == name) == []:
+                self.activities.insert(activity)
+            else:
+                self.activities.update(activity, self.query.name == activity["name"])
 
     def delete(self, name):
-        activities = self.activities.search(self.query.name == name)
-        if len(activities) > 0:
-            activity = activities[0]
-            if activity.get("default", False) is True:
-                self.activities.update({'default': True}, self.query.stock == True)
-            self.activities.remove(self.query.name == activity["name"])
+        with self.lock: 
+            activities = self.activities.search(self.query.name == name)
+            if len(activities) > 0:
+                activity = activities[0]
+                if activity.get("default", False) is True:
+                    self.activities.update({'default': True}, self.query.stock == True)
+                self.activities.remove(self.query.name == activity["name"])
 
     def list(self):
-        return self.activities.all()
+        with self.lock: 
+            return self.activities.all()
