f21ae0e354e4010e57a1a10ae4c9b0b2158007d9
previ <roberto.previtera@gmail.com>
2022-11-19 11:16:00 +0100

    disable HDMI and USB, LAN

--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-
-cd /coderbot && modprobe i2c-dev && python3 coderbot/main.py
+# disable ethernet, usb
+[ "$CODERBOT_disable_eth_usb" = "true" ] && echo '1-1' | tee /sys/bus/usb/drivers/usb/unbind
+# disable HDMI output
+/usr/bin/tvservice -o
+# enable i2c driver
+modprobe i2c-dev
+# set home
+cd /coderbot
+# start coderbot
+python3 coderbot/main.py
